1fc7c3e62ec469b0021598b9df27d80090526e8e
Sedilin <sramkowskilukasz@gmail.com>
2022-05-05 12:07:50 +0200

    Created a constructor

--- src/model/Booking.java
+++ src/model/Booking.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Booking {
+    private double totalPrice;
+    private boolean hasPaid;
+    private GuestList guestList;
+    private RoomList roomList;
+    private Date arrivalDate;
+    private Date departureDate;
+    private Room room;
+    private Guest guest;
+
+    public Booking (Room room, Date arrivalDate, Date departureDate, boolean hasPaid, Guest guest)
+    {
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
+        this.hasPaid = hasPaid;
+        this.room = room;
+        this.guest = guest;
+        totalPrice = arrivalDate.daysInBetween(departureDate) * room.getPrice();
+    }
+
+
 }
