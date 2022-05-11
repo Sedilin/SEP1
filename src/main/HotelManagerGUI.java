c9323663e5b9d9851074725c5c1c09763bd4a113
Sedilin <sramkowskilukasz@gmail.com>
2022-05-11 15:00:10 +0200

    Added main class launching the Hotel

---
+++ src/main/HotelManagerGUI.java
@@ -0,0 +1,10 @@
+package main;
+
+import javafx.application.Application;
+import view.StartApplication;
+
+public class HotelManagerGUI {
+    public static void main(String[] args) {
+        Application.launch(StartApplication.class);
+    }
+}
