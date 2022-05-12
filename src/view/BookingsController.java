1261c38c3cce52da33896a106db99ed462b83d35
Sedilin <sramkowskilukasz@gmail.com>
2022-05-12 14:11:59 +0200

    Updated things in Booking GUI

--- src/view/BookingsController.java
+++ src/view/BookingsController.java
@@ -1,5 +1,10 @@
 package view;
+import javafx.scene.layout.Region;
+import model.BookingModelManager;
+import model.Hotel;
 
 public class BookingsController
 {
+    private Region root;
+    private BookingModelManager modelManager;
 }
