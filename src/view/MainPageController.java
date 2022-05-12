addacbbda0023ae653fb888ec299dc5ad3d002a3
dianasuzana <dianastratan@gmail.com>
2022-05-12 16:44:04 +0200

    Updated the GUI for the room list page

--- src/view/MainPageController.java
+++ src/view/MainPageController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
+import main.HotelManagerGUI;
 import model.*;
 
 public class MainPageController
@@ -15,20 +16,48 @@ public class MainPageController
   private GuestList guestList;
   private BookingList bookingList;
 
-  //* Room list page
-
-  //
+  /**
+   * A GUI tab containing components for displaying a list of rooms.
+   * @author Diana Stratan
+   * @version 1.0
+   */
 
   @FXML private TextField searchField;
-  @FXML private TableView<Guest> guestListTable;
-  @FXML private TableColumn<Guest,String> RoomNoColumn;
-  @FXML private TableColumn<Guest,String> PriceColumn;
-  @FXML private TableColumn<Guest,String> CapacityColumn;
+  @FXML private TableView<Room> roomsListTable;
+  @FXML private TableColumn<Room,Integer> roomNoColumn;
+  @FXML private TableColumn<Room, Integer> priceColumn;
+  @FXML private TableColumn<Room, Integer> capacityColumn;
   @FXML private Button bookButton;
   @FXML private DatePicker arrivalDate;
   @FXML private DatePicker departureDate;
   @FXML private ToggleButton roomType;
   @FXML private Button searchButton;
   @FXML private TextField resetButton;
+
+  public void initialize() {
+    modelManager = new BookingModelManager("hotel.bin");
+    roomNoColumn.setCellValueFactory(new PropertyValueFactory<Room, Integer>("roomNumber"));
+    roomNoColumn.setPrefWidth(90);
+    priceColumn.setCellValueFactory(new PropertyValueFactory<Room, Integer>("price"));
+    priceColumn.setPrefWidth(90);
+    capacityColumn.setCellValueFactory(new PropertyValueFactory<Room, Integer>("capacity"));
+    capacityColumn.setPrefWidth(72);
+
+    updateRoomsArea();
+  }
+
+
+
+
+  public void updateRoomsArea()
+  {
+    roomsListTable.getItems().clear();
+    RoomList rooms = modelManager.load().getAllRooms();
+
+    for(int i = 0; i<rooms.size(); i++)
+    {
+      roomsListTable.getItems().add(rooms.getRoom(i));
+    }
+  }
 
 }
