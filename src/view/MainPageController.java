a2a8b6eb9e845f2f91ba4c931dbbb858d4c5f936
dianasuzana <dianastratan@gmail.com>
2022-05-12 15:36:23 +0200

    Update of GUI MainPage Controller

--- src/view/MainPageController.java
+++ src/view/MainPageController.java
@@ -1,5 +1,34 @@
 package view;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
+import model.*;
 
 public class MainPageController
 {
+  private Region root;
+  private BookingModelManager modelManager;
+  private RoomList roomList;
+  private GuestList guestList;
+  private BookingList bookingList;
+
+  //* Room list page
+
+  //
+
+  @FXML private TextField searchField;
+  @FXML private TableView<Guest> guestListTable;
+  @FXML private TableColumn<Guest,String> RoomNoColumn;
+  @FXML private TableColumn<Guest,String> PriceColumn;
+  @FXML private TableColumn<Guest,String> CapacityColumn;
+  @FXML private Button bookButton;
+  @FXML private DatePicker arrivalDate;
+  @FXML private DatePicker departureDate;
+  @FXML private ToggleButton roomType;
+  @FXML private Button searchButton;
+  @FXML private TextField resetButton;
+
 }
