a5b926bf91e150e3f1b041879984b1310727371c
Sedilin <sramkowskilukasz@gmail.com>
2022-05-18 10:39:49 +0200

    Version without smoking functions

--- src/view/RegisterGuestDetailsController.java
+++ src/view/RegisterGuestDetailsController.java
@@ -1,10 +1,7 @@
 package view;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import model.*;
@@ -34,6 +31,8 @@ public class RegisterGuestDetailsController
   @FXML private Button importGuestDetails;
 
   private Guest currentGuest;
+  private int count = 0;
+  Guest additionalGuest;
 
   //taking info from this classes
   public void init(ViewHandler viewHandler, BookingModelManager modelManager, Region root)
@@ -61,6 +60,7 @@ public class RegisterGuestDetailsController
   {
     if (e.getSource() == checkIn)
     {
+      count = 0;
       Hotel hotel = modelManager.load();
       hotel.getAllBookings().getBooking(viewHandler.getMainPageController().bookingListTable.getSelectionModel().getSelectedItem()).setCheckedIn();
       modelManager.save(hotel);
@@ -88,18 +88,49 @@ public class RegisterGuestDetailsController
     }
     else if (e.getSource() == saveButton)
     {
-      Hotel hotel = modelManager.load();
-      currentGuest.setId(ID.getText());
-      currentGuest.setNationality(nationality.getText());
-      currentGuest.setAddress(new Address(postalCode.getText(), city.getText(), country.getText()));
-      String[] tempArr = birthday.getText().split("/");
-      Date birthday = new Date(Integer.parseInt(tempArr[0]),Integer.parseInt(tempArr[1]), Integer.parseInt(tempArr[2]));
-      currentGuest.setBirthday(birthday);
-      hotel.addGuest(currentGuest);
-      modelManager.save(hotel);
-      System.out.println(modelManager.load().getAllGuests());
+      if (count==0)
+      {
+        Hotel hotel = modelManager.load();
+        currentGuest.setId(ID.getText());
+        currentGuest.setNationality(nationality.getText());
+        currentGuest.setAddress(new Address(postalCode.getText(), city.getText(), country.getText()));
+        String[] tempArr = birthday.getText().split("/");
+        Date birthday = new Date(Integer.parseInt(tempArr[0]),Integer.parseInt(tempArr[1]), Integer.parseInt(tempArr[2]));
+        currentGuest.setBirthday(birthday);
+        hotel.addGuest(currentGuest);
+        modelManager.save(hotel);
+        count++;
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "The main guest has been added.");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+
+      }
+      else if (count <= viewHandler.getMainPageController().bookingListTable.getSelectionModel().getSelectedItem().getRoom().getCapacity())
+        {
+          Hotel hotel = modelManager.load();
+          additionalGuest = new Guest(firstName.getText(), lastName.getText(), phoneNumber.getText());
+          additionalGuest.setId(ID.getText());
+          additionalGuest.setNationality(nationality.getText());
+          additionalGuest.setAddress(new Address(postalCode.getText(), city.getText(), country.getText()));
+          String[] tempArr = birthday.getText().split("/");
+          Date birthday = new Date(Integer.parseInt(tempArr[0]),Integer.parseInt(tempArr[1]), Integer.parseInt(tempArr[2]));
+          additionalGuest.setBirthday(birthday);
+          hotel.addGuest(additionalGuest);
+          modelManager.save(hotel);
+          count++;
+          Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "The additional guest has been added. \nNumber of guests created for the booking: " + count);
+          alert.setHeaderText(null);
+          alert.showAndWait();
+        }
+      else
+        {
+          Alert alert = new Alert(Alert.AlertType.ERROR, "The room has reached its max capacity!");
+          alert.setHeaderText(null);
+          alert.showAndWait();
+        }
+
+      }
     }
-  }
   private void updateRegisterGuestDetails()
   {
     firstName.clear();
